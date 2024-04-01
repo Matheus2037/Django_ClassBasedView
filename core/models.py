b7c984d1e59b4bef95e902dc32a46d5be940857f
Matheus Silva Gastaldi <matheusdasilvagastaldi@gmail.com>
2024-03-31 22:57:19 -0300

    primeiro commit

---
+++ core/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from stdimage import StdImageField
+
+
+class Base(models.Model):
+
+    criados = models.DateField('Criação', auto_now_add=True)
+    modificado = models.DateField('Atualização', auto_now=True)
+    ativo = models.BooleanField("Ativo?" , default=True)
+
+    class Meta:
+        abstract = True
+
+class Servicos(Base):
+    ICONE_CHOICE = (
+        ('lni-cog', 'Engrenagem'),
+        ('lni-stats-up', 'Gráfico'),
+        ('lni-users', 'Usuários'),
+        ('lni-layers', 'Design'),
+        ('lni-mobile', 'Mobile'),
+        ('lni-rocket', 'Foguete'),
+    )
+    servico = models.CharField("Serviço", max_length=100)
+    descricao = models.TextField("Descrição", max_length=200)
+    icone = models.CharField("Icone", max_length=12, choices=ICONE_CHOICE)
+
+    class Meta:
+        verbose_name = "Serviço"
+        verbose_name_plural = "Serviços"
+    def __str__(self):
+        return self.servico
+
+
+class Cargo(Base):
+    cargo = models.CharField("Cargo", max_length=100)
+
+    class Meta:
+        verbose_name = "Cargo"
+        verbose_name_plural = "Cargos"
+
+    def __str__(self):
+        return self.cargo
+
+class Equipe(base):
+    nome = models.CharField("Nome", max_length=100)
+    cargo = models.ForeignKey("core.Cargo", verbose_name="Cargo", on_delete=models.CASCADE)
+    bio = models.TextField("Bio", max_length=200)
+    imagem = StdImageField('Imagem', upload_to=equipe, variations={'thumb': {'width': 480, 'height': 480, 'crop': True}})
