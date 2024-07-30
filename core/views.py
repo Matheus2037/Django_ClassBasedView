d15486dd2ba013cfb3f0c00b1a4c099591bbcb18
Matheus da Silva Gastaldi <matheusdasilvagastaldi@gmail.com>
2024-07-29 23:31:17 -0300

    Alterando o arquivo views.py
    O gerenciamento de contexto é feito no arquivo view.py

--- core/views.py
+++ core/views.py
@@ -1,6 +1,11 @@
 from django.views.generic import TemplateView
 
+from .models import Equipe, Servicos
 class IndexView(TemplateView):
     template_name = 'index.html'
 
-
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['servicos'] = Servicos.objects.order_by('?').all()
+        context['equipe'] = Equipe.objects.order_by('?').all()
+        return context
