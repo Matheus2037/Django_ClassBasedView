090a0ce99f311723c8dfc03fd0fb3f67bb99a318
Matheus Silva Gastaldi <matheusdasilvagastaldi@gmail.com>
2024-08-18 18:50:55 -0300

    Feito teste para funcao get_file_path

---
+++ core/tests/test_models.py
@@ -0,0 +1,14 @@
+import uuid
+from django.test import TestCase
+from model_mommy import mommy
+
+from core.models import get_file_path
+
+class GetFilePathTestCase(TestCase):
+
+    def setUp(self):
+        self.filename = f'{uuid.uuid4()}.png'
+
+    def test_get_file_path(self):
+        arquivo = get_file_path(None, 'teste.png')
+        self.assertTrue(len(arquivo), len(self.filename))
